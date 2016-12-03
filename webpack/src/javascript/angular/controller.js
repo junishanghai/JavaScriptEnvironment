12b887674f188d5b4d46386a96a6ab269baa8710
yohei takara <youhei.takara@bluestar.jpn.com>
2016-12-03 23:22:31 +0900

    angular module 名タイポ修正

--- webpack/src/javascript/angular/controller.js
+++ webpack/src/javascript/angular/controller.js
@@ -3,7 +3,7 @@ require('./config');
 require('./factory');
 require('./filter');
 
-angModule.billApp.controller("webpackTestCtrl", ['$scope', 'DemoFactory', 'HttpResponseFactory', function($scope, DemoFactory, HttpResponseFactory) {
+angModule.angApp.controller("webpackTestCtrl", ['$scope', 'DemoFactory', 'HttpResponseFactory', function($scope, DemoFactory, HttpResponseFactory) {
     $scope.init = function() {
 
     };
